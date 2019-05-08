7b07d6bc051ca499c82c4efdbabb0c8a46de9b92
Kenta Kusumoto <kenta.kusumoto@futurice.com>
2019-05-08 12:27:56 +0300

    Change OmmAlertHandlerTest to test new methods and data structures

--- src/test/java/fi/hsl/transitdata/omm/OmmAlertHandlerTest.java
+++ src/test/java/fi/hsl/transitdata/omm/OmmAlertHandlerTest.java
@@ -1,7 +1,6 @@
 package fi.hsl.transitdata.omm;
 
-import com.google.transit.realtime.GtfsRealtime;
-import fi.hsl.common.gtfsrt.FeedMessageFactory;
+import fi.hsl.common.transitdata.proto.InternalMessages;
 import fi.hsl.transitdata.omm.db.MockOmmConnector;
 import fi.hsl.transitdata.omm.models.AlertState;
 import fi.hsl.transitdata.omm.models.Bulletin;
@@ -19,80 +18,72 @@ public class OmmAlertHandlerTest {
     final static String TIMEZONE = "Europe/Helsinki";
 
     private MockOmmConnector readDefaultMockData() throws Exception {
-        return MockOmmConnector.newInstance("2019_04_alert_dump.tsv");
+        return MockOmmConnector.newInstance("2019_05_alert_dump.tsv");
     }
 
-    private List<GtfsRealtime.FeedEntity> createFeedEntitiesFromDefaultMockData() throws Exception {
+    private InternalMessages.ServiceAlert createServiceAlertFromDefaultMockData() throws Exception {
         MockOmmConnector connector = readDefaultMockData();
         List<Bulletin> bulletins = connector.getBulletinDAO().getActiveBulletins();
         Map<Long, Line> lines = connector.getLineDAO().getAllLines();
         Map<Long, StopPoint> stops = connector.getStopPointDAO().getAllStopPoints();
 
-        List<GtfsRealtime.FeedEntity> feedEntities = OmmAlertHandler.createFeedEntities(bulletins, lines, stops, TIMEZONE);
-        assertEquals(bulletins.size(), feedEntities.size());
-        validateMockDataFirstEntity(feedEntities.get(0));
-        return feedEntities;
+        final InternalMessages.ServiceAlert alert = OmmAlertHandler.createServiceAlert(bulletins, lines, stops, TIMEZONE);
+        assertEquals(bulletins.size(), alert.getBulletinsCount());
+        validateMockDataFirstEntity(alert.getBulletinsList().get(0));
+        return alert;
     }
 
     @Test
-    public void testCreateFeedEntities() throws Exception {
-        List<GtfsRealtime.FeedEntity> feedEntities = createFeedEntitiesFromDefaultMockData();
+    public void testCreateServiceAlert() throws Exception {
+        InternalMessages.ServiceAlert alert = createServiceAlertFromDefaultMockData();
 
-        feedEntities.forEach(entity -> {
-            assertTrue(entity.hasAlert());
-            assertTrue(entity.hasId());
+        alert.getBulletinsList().forEach(bulletin -> {
+            assertTrue(bulletin.hasBulletinId());
+            assertTrue(bulletin.hasImpact());
+            assertTrue(bulletin.hasCategory());
+            assertTrue(bulletin.hasPriority());
+            assertTrue(bulletin.hasLastModifiedUtcMs());
+            assertTrue(bulletin.hasValidFromUtcMs());
+            assertTrue(bulletin.hasValidToUtcMs());
+            assertEquals(3, bulletin.getTitlesCount());
+            assertEquals(3, bulletin.getDescriptionsCount());
 
-            assertFalse(entity.hasTripUpdate());
-            assertFalse(entity.hasVehicle());
-
-            GtfsRealtime.Alert alert = entity.getAlert();
-            assertTrue(alert.hasCause());
-            assertTrue(alert.hasEffect());
-            assertTrue(alert.hasDescriptionText());
-            assertTrue(alert.hasHeaderText());
-            assertTrue(alert.hasSeverityLevel());
-
+            if (bulletin.getAffectedRoutesCount() > 0) {
+                bulletin.getAffectedRoutesList().forEach(entity -> {
+                    assertTrue(entity.hasEntityId());
+                });
+            }
+            if (bulletin.getAffectedStopsCount() > 0) {
+                bulletin.getAffectedStopsList().forEach(entity -> {
+                    assertTrue(entity.hasEntityId());
+                });
+            }
         });
     }
 
-    @Test
-    public void testCreateFeedMessage() throws Exception {
-        List<GtfsRealtime.FeedEntity> feedEntities = createFeedEntitiesFromDefaultMockData();
-        final long timestamp = System.currentTimeMillis() / 1000;
+    private void validateMockDataFirstEntity(final InternalMessages.Bulletin bulletin) {
+        assertEquals("6340", bulletin.getBulletinId());
+        assertTrue(bulletin.hasImpact());
+        assertTrue(bulletin.hasCategory());
+        assertTrue(bulletin.hasPriority());
+        assertEquals(InternalMessages.Bulletin.Impact.POSSIBLE_DEVIATIONS, bulletin.getImpact());
+        assertEquals(InternalMessages.Category.NO_TRAFFIC_DISRUPTION, bulletin.getCategory());
+        assertEquals(InternalMessages.Bulletin.Priority.INFO, bulletin.getPriority());
 
-        GtfsRealtime.FeedMessage msg = FeedMessageFactory.createFullFeedMessage(feedEntities, timestamp);
+        assertTrue(bulletin.hasAffectsAllRoutes());
+        assertTrue(bulletin.hasAffectsAllStops());
+        assertFalse(bulletin.getAffectsAllRoutes());
+        assertFalse(bulletin.getAffectsAllStops());
 
-        assertNotNull(msg);
-        assertEquals(timestamp, msg.getHeader().getTimestamp());
-        assertEquals(feedEntities.size(), msg.getEntityCount());
+        assertEquals(3, bulletin.getAffectedRoutesCount());
+        assertEquals(0, bulletin.getAffectedStopsCount());
 
-        validateMockDataFirstEntity(msg.getEntity(0));
-    }
+        InternalMessages.Bulletin.AffectedEntity route = bulletin.getAffectedRoutes(0);
+        assertTrue(route.hasEntityId());
+        assertEquals(MockOmmConnector.lineGidToLineId(9011301065700000L), route.getEntityId());
 
-    private void validateMockDataFirstEntity(GtfsRealtime.FeedEntity entity) {
-        assertTrue(entity.hasAlert());
-        assertFalse(entity.hasTripUpdate());
-        assertFalse(entity.hasVehicle());
-        assertFalse(entity.hasIsDeleted());
-
-        assertEquals("6237", entity.getId());
-        GtfsRealtime.Alert alert = entity.getAlert();
-        assertNotNull(alert);
-        assertEquals(GtfsRealtime.Alert.Effect.OTHER_EFFECT, alert.getEffect());
-        assertEquals(GtfsRealtime.Alert.Cause.OTHER_CAUSE, alert.getCause());
-        assertEquals(1, alert.getActivePeriodCount());
-        assertEquals(4, alert.getInformedEntityList().size());
-        assertEquals(GtfsRealtime.Alert.SeverityLevel.INFO, alert.getSeverityLevel());
-
-        GtfsRealtime.EntitySelector selector = alert.getInformedEntity(0);
-        assertFalse(selector.hasTrip());
-        assertFalse(selector.hasStopId());
-        assertFalse(selector.hasAgencyId());
-        assertTrue(selector.hasRouteId());
-        assertEquals(MockOmmConnector.lineGidToLineId(9011301067600000L), selector.getRouteId());
-
-        GtfsRealtime.TranslatedString header = alert.getHeaderText();
-        assertEquals(3, header.getTranslationCount());
+        assertEquals(3, bulletin.getTitlesCount());
+        assertEquals(3, bulletin.getDescriptionsCount());
     }
 
     @Test
@@ -101,72 +92,76 @@ public class OmmAlertHandlerTest {
         List<Bulletin> bulletins = connector.getBulletinDAO().getActiveBulletins();
         AlertState state = new AlertState(bulletins);
         long utcMs = OmmAlertHandler.lastModifiedInUtcMs(state, TIMEZONE);
-        assertEquals(1554726084480L, utcMs);
+        assertEquals(1557228675107L, utcMs);
     }
 
     @Test
-    public void testOneFeedEntityThoroughly() throws Exception {
-        MockOmmConnector connector = readDefaultMockData();
-        List<Bulletin> bulletins = connector.getBulletinDAO().getActiveBulletins();
-        Map<Long, Line> lines = connector.getLineDAO().getAllLines();
-        Map<Long, StopPoint> stops = connector.getStopPointDAO().getAllStopPoints();
+    public void testOneBulletinThoroughly() throws Exception {
+        final MockOmmConnector connector = readDefaultMockData();
+        final List<Bulletin> bulletins = connector.getBulletinDAO().getActiveBulletins();
+        final Map<Long, Line> lines = connector.getLineDAO().getAllLines();
+        final Map<Long, StopPoint> stops = connector.getStopPointDAO().getAllStopPoints();
 
-        List<GtfsRealtime.FeedEntity> feedEntities = OmmAlertHandler.createFeedEntities(bulletins, lines, stops, TIMEZONE);
-        Optional<GtfsRealtime.FeedEntity> maybeEntity = feedEntities.stream().filter(entity -> entity.getId().equals("6298")).findFirst();
-        assertTrue(maybeEntity.isPresent());
+        final Optional<Bulletin> maybeSelectedBulletin = bulletins.stream().filter(b -> b.id == 6431).findFirst();
+        assertTrue(maybeSelectedBulletin.isPresent());
+        final Bulletin selectedBulletin = maybeSelectedBulletin.get();
 
-        GtfsRealtime.Alert alert = maybeEntity.get().getAlert();
+        final Optional<InternalMessages.Bulletin> maybeBulletin = OmmAlertHandler.createBulletin(selectedBulletin, lines, stops, TIMEZONE);
+        assertTrue(maybeBulletin.isPresent());
+        final InternalMessages.Bulletin bulletin = maybeBulletin.get();
 
-        assertEquals(Bulletin.Category.NO_TRAFFIC_DISRUPTION.toGtfsCause(), alert.getCause());
-        assertEquals(Bulletin.Impact.DEVIATING_SCHEDULE.toGtfsEffect(), alert.getEffect());
-        assertEquals(1, alert.getActivePeriodCount());
-        assertEquals( 1555304400L, alert.getActivePeriod(0).getStart());
-        assertEquals( 1556625600L, alert.getActivePeriod(0).getEnd());
-        assertEquals(2, alert.getInformedEntityCount());
-        assertEquals(Bulletin.Priority.INFO.toGtfsSeverityLevel().get(), alert.getSeverityLevel());
+        assertEquals(InternalMessages.Bulletin.Impact.DISRUPTION_ROUTE, bulletin.getImpact());
+        assertEquals(InternalMessages.Category.ROAD_CLOSED, bulletin.getCategory());
+        assertEquals(InternalMessages.Bulletin.Priority.INFO, bulletin.getPriority());
 
-        List<GtfsRealtime.EntitySelector> entities = alert.getInformedEntityList();
-        validateEntitySelector(entities.get(0),  MockOmmConnector.lineGidToLineId(9011301004400000L));
-        validateEntitySelector(entities.get(1),  MockOmmConnector.lineGidToLineId(9011301004500000L));
+        assertEquals(1557228675107L, bulletin.getLastModifiedUtcMs());
+        assertEquals(1557885600000L, bulletin.getValidFromUtcMs());
+        assertEquals(1558198800000L, bulletin.getValidToUtcMs());
 
-        GtfsRealtime.TranslatedString description = alert.getDescriptionText();
-        assertEquals(3, description.getTranslationCount());
-        description.getTranslationList().forEach(translation -> {
-            switch (translation.getLanguage()) {
-                case "fi": assertEquals("Korkeasaareen ajetaan lisälähtöjä linjoilla 16B ja 16X klo 8 – 15 ajalla 15.4. - 30.4. //Info: hsl.fi", translation.getText());
+        assertTrue(bulletin.hasAffectsAllRoutes());
+        assertTrue(bulletin.hasAffectsAllStops());
+        assertFalse(bulletin.getAffectsAllRoutes());
+        assertFalse(bulletin.getAffectsAllStops());
+
+        assertEquals(0, bulletin.getAffectedRoutesCount());
+        assertEquals(5, bulletin.getAffectedStopsCount());
+
+        long[] affectedStopGids = {
+                9022301110755001L,
+                9022301110755002L,
+                9022301110754001L,
+                9022301110792001L,
+                9022301110793001L
+        };
+        for (int i = 0; i < bulletin.getAffectedStopsCount(); ++i) {
+            final InternalMessages.Bulletin.AffectedEntity stop = bulletin.getAffectedStops(i);
+            assertTrue(stop.hasEntityId());
+            assertEquals(MockOmmConnector.stopGidtoStopPointId(affectedStopGids[i]), stop.getEntityId());
+        }
+
+        assertEquals(3, bulletin.getTitlesCount());
+        bulletin.getTitlesList().forEach(title -> {
+            switch (title.getLanguage()) {
+                case "fi": assertEquals("Hämeentie suljettu 18.5. Arabian katufestivaalin ", title.getText());
                     break;
-                case "sv": assertEquals("Linjerna 16B och 16X kör extra avgångar till Högholmen kl. 8-15 under perioden 15.-30.4. //Info: hsl.fi.", translation.getText());
+                case "sv": assertEquals("Gatan avstängd", title.getText());
                     break;
-                case "en": assertEquals("Bus routes 16B and 16X will provide additional service to Helsinki Zoo in Korkeasaari between 15 April and 30 April. //Info: hsl.fi.", translation.getText());
+                case "en": assertEquals("Road closed", title.getText());
                     break;
                 default: assertTrue(false);
             }
         });
-
-        GtfsRealtime.TranslatedString header = alert.getHeaderText();
-        assertEquals(3, header.getTranslationCount());
-        header.getTranslationList().forEach(translation -> {
-            switch (translation.getLanguage()) {
-                case "fi": assertEquals("Korkeasaaren lisäliikenne 15.-30.4.", translation.getText());
+        assertEquals(3, bulletin.getDescriptionsCount());
+        bulletin.getDescriptionsList().forEach(description -> {
+            switch (description.getLanguage()) {
+                case "fi": assertEquals("Linjat 52, 55, 71, 78N ja 506 Arabiassa poikkeusreiteillä la 18.5. klo 9-20. /Info: hsl.fi.", description.getText());
                     break;
-                case "sv": assertEquals("Inga trafikstörningar", translation.getText());
+                case "sv": assertEquals("Linjerna 52, 55, 71, 78N och 506 kör avvikande rutter i Arabia 18.5 kl. 9-20. /Info: hsl.fi/sv", description.getText());
                     break;
-                case "en": assertEquals("No traffic disruption", translation.getText());
+                case "en": assertEquals("Buses 52, 55, 71, 78N and 506 diverted in Arabia on 18 May 9am-8pm. /Info: hsl.fi/en", description.getText());
                     break;
                 default: assertTrue(false);
             }
         });
-
     }
-
-    private void validateEntitySelector(GtfsRealtime.EntitySelector entity, String id) {
-        assertFalse(entity.hasAgencyId());
-        assertFalse(entity.hasStopId());
-        assertTrue(entity.hasRouteId());
-        assertFalse(entity.hasRouteType());
-        assertFalse(entity.hasTrip());
-        assertEquals(id, entity.getRouteId());
-    }
-
-
 }
